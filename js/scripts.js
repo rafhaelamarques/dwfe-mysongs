c53bbb59d995ea24c1e1d5c070649866b97bb7ba
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2022-09-10 17:10:13 -0300

    cadastro de artista

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function show(){
+    document.getElementById("popup").style.display == 'block' ? document.getElementById("popup").style.display = 'none' : document.getElementById("popup").style.display = 'block';
+}
+function hide(){
+    document.getElementById("popup").style.display = 'none';
+}
